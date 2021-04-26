7bb207fd5816f2139b676a9d7acc5bafd2b31b72
tymchii.a <offersbox@gmail.com>
2021-04-26 14:53:09 +0300

    separated components, props

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import './menu-item.component.scss';
+
+const MenuItem = ({ title, id, imageUrl, size }) => (
+  <div
+    style={{ backgroundImage: `url(${imageUrl})` }}
+    className={`${size} menu-item`}
+    key={id}
+  >
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">Shop now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
