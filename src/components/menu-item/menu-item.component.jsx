8fcd8ba49e3e3b0b007539747776b19846e1ba24
tymchii.a <offersbox@gmail.com>
2021-04-26 15:48:40 +0300

    hover effect

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,11 @@
 import './menu-item.component.scss';
 
 const MenuItem = ({ title, id, imageUrl, size }) => (
-  <div
-    style={{ backgroundImage: `url(${imageUrl})` }}
-    className={`${size} menu-item`}
-    key={id}
-  >
+  <div className={`${size} menu-item`} key={id}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">Shop now</span>
