ed366bb460cf676023ff43bb46fa2320fa60cf3b
tymchii.a <offersbox@gmail.com>
2021-05-12 09:54:11 +0300

    signing site bottom

--- src/App.js
+++ src/App.js
@@ -4,28 +4,31 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-up/sign-in-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component'
+import CheckoutPage from './pages/checkout/checkout.component';
 import { useSelector } from 'react-redux';
 
 function App() {
   const currentUser = useSelector(state => state.user.currentUser);
 
   return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/checkout" component={CheckoutPage} />
-        <Route
-          exact
-          path="/signin"
-          render={() =>
-            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-          }
-        />
-      </Switch>
-    </div>
+    <>
+      <div className="root-container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+            }
+          />
+        </Switch>
+      </div>
+      <div id="detonatorx">detonatorx ©, 2021</div>
+    </>
   );
 }
 
